b355d40990e2bb88dfb6281193e7805a3989c767
James <gaojun1986@gmail.com>
2016-06-20 10:57:41 +0800

    Interview Questions Project

---
+++ Interview/src/ReverseList/ListNode.java
@@ -0,0 +1,22 @@
+package ReverseList;
+
+public class ListNode {
+	private String value;
+	private ListNode next;
+	public ListNode(String v, ListNode n) {
+		setValue(v);
+		setNext(n);
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public ListNode getNext() {
+		return next;
+	}
+	public void setNext(ListNode next) {
+		this.next = next;
+	}
+}
---
+++ Interview/src/ReverseList/MainApp.java
@@ -0,0 +1,47 @@
+package ReverseList;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		int a[] = {1,2,3,4,5,6,7,8,9};
+		
+		ListNode head = createNode(a, 0);
+		doPrint(head);
+		System.out.println("=======Reverse=======");
+		doPrint(doReverse(head));
+		
+	}
+	
+	private static void doPrint(ListNode node) {
+		if(node == null)
+			return ;
+		System.out.println(node.getValue());
+		doPrint(node.getNext());
+	}
+	
+	public static ListNode createNode(int[] a, int index) {
+		if(index >= a.length)
+			return null;
+		ListNode next = createNode(a, index + 1);
+		return new ListNode(String.valueOf(a[index]), next);
+	}
+	
+	
+	static ListNode next = null;
+	static ListNode prev = null;
+	static ListNode newHead = null;
+	public static ListNode doReverse(ListNode node) {
+		next = null;
+		prev = null;
+		while(node != null) {
+			next = node.getNext();
+			node.setNext(prev);
+			
+			prev = node;
+			node = next;
+		}
+		
+		return prev; //new Head
+	}
+
+}
